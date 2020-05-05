f8e7bd29a5d45cfd07603280595dd93ac7b911b6
ndresgarc <belsierre@gmail.com>
2020-05-05 15:21:54 +0200

    package.json, dummy server.js

--- endpoint.js
+++ endpoint.js
@@ -4,6 +4,9 @@
 /* REQUIRE */
 const ws = require('ws');
 const fs = require('fs');
+const http = require('http');
+const httpProxy = require('http-proxy');
+
 
 /* GLOBAL */
 let config = {};
@@ -17,6 +20,24 @@ fs.readFile('config.json', (error, data) => {
 });
 
 /* INIT */
+
+var server = http.createServer(function(req, res) {
+  console.log(req.url);
+  socket.send(config.endpoint.id + '-' + req.url);
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+
+  
+  /* WEBSOCKET CODE EXECUTION HERE */
+  // proxy.web(req, res, { target: 'http://127.0.0.1:9000' });
+  /* WEBSOCKET CODE EXECUTION HERE */
+
+}).listen(10001);
+
+console.log("listening on port 10001");
+
 function init(config) {
   socket = new ws(config.websocket.url);
   console.log(config);
@@ -44,4 +65,7 @@ function init(config) {
 function handler() {
   
 }
+
+
+
 
--- server.js
+++ server.js
@@ -1,41 +1,20 @@
+
 const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ port: 80 });
  
 function getWeb() {
-
     return "Your web response";
-
 }
-
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`);
-
-    if (message == '2-get-web') {
+    
         wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send('message for 1');
-              }
+          
+                client.send(message);
+              
           });
-    }
-
-    if (message == '1-get-web') {
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send('message for 2');
-              }
-          });
-    }
-
-    if (message.indexOf('get-web') > -1) {
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send('message for everyone');
-              }
-          });
-    }
-
   })
   ws.send('Hello! Message From Server!!');
 });
